46482398cb20459f41510066a788cdfda44284ac
guillermooo <guillermo.lopez@outlook.com>
2018-06-10 01:18:45 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# plugin-surround
-In-progress surround plugin for Six
+# Surround --- The First Sublime Six Plugin
+
+Things are bound to break along the way, but in the spirit of 'release early and release often'...
+
+# How to Use
+
+Six needs to find this plugin to register it. To make it happen:
+
+1. Copy the `.py` files in this repo to `Packages\User\six`
+2. Create a `sixrc.py` file in `Packages\User`
+
+Include something like this in `sixrc.py` (inspired by my own):
+
+```python
+import sublime
+
+# This line imports our custom Sublime Text command and registers our Six plugin.
+# BTW, this will fail if Six isn't available. We need to cover that case one day...
+from User.six.surround import *
+
+
+def plugin_loaded():
+    # Everything below this line is extra stuff that you can keep in this file too.
+    # Just an example; follow your heart!
+    settings = sublime.load_settings('Preferences.sublime-settings')
+
+    # Define mappings if Six is enabled.
+    if 'Six' not in settings.get('ignored_packages'):
+        from Six._init_ import editor
+        from Six.lib.constants import Mode
+
+        editor.mappings.add(Mode.Normal, 'Y', 'y$')
+        editor.mappings.add(Mode.Normal, '<Space>', ':')
+        editor.mappings.add(Mode.Normal, ',pp', 'a()<Esc>ha')
+```
---
+++ surround.py
@@ -0,0 +1,122 @@
+# TODO: Add tests and CI integration.
+import logging
+
+import sublime
+import sublime_plugin
+
+from Six._init_ import editor
+from Six.lib.constants import Mode
+from Six.lib.errors import AbortCommandError
+from Six.lib.operators_internal import OperatorWithoutMotion
+
+
+# Hook ourselves up to the Six logger. Anyhing prefixed with 'Six.' is fine,
+# but let's establish a standard (there's a 'plugin' folder in Six, hence
+# 'user.plugin' here.')
+_logger = logging.getLogger('Six.user.plugin.surround')
+
+# Limit stuff that gets exported to global scope; our TextCommand needs to be
+# available to Sublime Text.
+__all__ = (
+    '_six_surround_change',
+    )
+
+
+# TODO: use cs for keys instead when c can act as a namespace.
+# Register this command for the given mode and assign it the given key sequence.
+@editor.register(mode=Mode.Normal, keys="zs")
+# Our command doesn't need a motion; it's implicit.
+class SurroundChangeSixPlugin(OperatorWithoutMotion):
+
+    def __init__(self, *args, **kwargs):
+        # Give our command a name to satisfy the base class.
+        super().__init__('zs', *args, **kwargs)
+        # We want to replace this delimiter...
+        self.old = None
+        # ... with this other delimiter.
+        self.new = None
+
+    # We shouldn't need to implement this as an operator. But because we are
+    # technically inside an OperatorOrMotion (z), we must, at least for now.
+    def set_parent(self, parent):
+        pass
+
+    def process(self, mode, state):
+        # If you run this, you will see how the keys build up as you press them.
+        _logger.info("processing keys... %s" % state.keys)
+        # Let OperatorWithoutMotion do its part. Since most commands of this
+        # kind do the same thing, the behavior is encapsulated in the base
+        # class. But we are a bit of a snowflake, so we need to adjust a few
+        # things below.
+        super().process(mode, state)
+
+        # We need to collect two keys, one for the old delimiter; the other
+        # for the new.
+        for i in range(2):
+            # 'state', aka 'command state', gives us useful information about
+            # the in-flight command. And mutating its fields, we can communicate
+            # back with the Editor in charge of managing us should we need to.
+            if state.is_at_eof:
+                # No more keys from user available -- request more.
+                state.more_input = True
+                return
+
+            # The key index is incremented for us as keys are consumed while
+            # the command is processed upstream. At this point, zs have been
+            # consumed. Let's satisfy our requirements now if we can.
+            key = state.next()
+
+            # TODO: support more delimiters.
+            if key not in ('"', "'"):
+                # Let the Editor know that something went wrong. Maybe this
+                # isn't the best error to raise here, but it'll do for now.
+                raise AbortCommandError
+
+            if i == 0:
+                # First available key.
+                self.old = key
+            else:
+                # Second available key.
+                self.new = key
+
+        # Done! The command is ready to be executed next.
+        state.more_input = False
+
+    def execute(self, mode, times, register):
+        if self.old == self.new:
+            # User is tired. Stop. We could complain too; not sure what the
+            # actual Vim Surround plugin does.
+            _logger.info("no change needed; stop here")
+        else:
+            # This is a bit ugly and Six should help us hide these details,
+            # but that isn't ready yet.
+            view = sublime.active_window().active_view()
+            # The processing Six needs to do before we can actually change
+            # the Sublime Text state is done. Delegate to an ST command. After
+            # we've done our thing in ST, Six will run the Six command's
+            # lifecycle to its end (mainly calling our .reset() below and
+            # cleaning up the global Six state).
+            view.run_command('_six_surround_change', {
+                'from_': self.old,
+                'to': self.new
+                })
+
+    def reset(self):
+        # The Six Editor will call us to give us a chance to clean up after
+        # we have been executed.
+        _logger.info("resetting...")
+        super().reset()
+        self.old = None
+        self.new = None
+
+
+class _six_surround_change(sublime_plugin.TextCommand):
+    """Replaces delimiters.
+
+    For example, zs'" replaces (') with (") if we are currently inside a string
+    delimited by (').
+    """
+    def run(self, edit, from_, to):
+        # The drudgery above is necessary only to reach this point, where we
+        # know exactly what Sublime Text needs to do. Now we have to implement it.
+        _logger.info("doing the heavy lifting here... replacing {} with {}".format(from_, to))
